87d2c2e21592bf84e6b9112f5cc7e82cafee1f73
JhonDavidHernandezCampus <yonif975@gmail.com>
2023-03-07 13:40:01 -0500

    feat: {Metodo toLowerCase() }

--- documentacion.js
+++ documentacion.js
@@ -1,14 +1,13 @@
-/* Método repeat()
+/* Metodo toLowerCase()
 
-El método repeat() construye y devuelve una nueva cadena que contiene el número especificado de
-copias de la cadena en la cual fue llamada, concatenados.
+Me regresa la cadena en Mayuscula
+
    Ejemplo */
 
-alert("El metodo repeat me multiplica la cadena de texto las veces que le digamos y me la regresa como una sola cadana:)")
+alert(" Me convierte toda la cadena a mayuscula:)")
 let cadena =  prompt("Ingrese una cadena de texto:")
-let ini = prompt(`ingrese el numero de veces que desea repetir la cadena`)
 
 
-alert(`La cadena queda de la siguiente forma : \n  ${cadena.repeat(ini)}`)
+alert(`La cadena queda de la siguiente forma : \n  ${cadena.toLocaleUpperCase()}`)
 
 
