787857c32db3505f4c116a9370d381e5a0f1d118
JhonDavidHernandezCampus <yonif975@gmail.com>
2023-03-07 09:37:47 -0500

    feat(create a new scope): :art:

---
+++ documentacion.js
@@ -0,0 +1,11 @@
+/* Método length 
+
+Indica el número de unidades en la cadena. 
+   Ejemplo */
+
+alert("El metodo length Indica el número de unidades en la cadena(con espacios incluidos)")
+let cadena =  prompt("Ingrese una cadena de texto:")
+
+alert(`La cantidad de caracteres que tiene la cadena \n ingresada anterior mente son:  ${cadena.length}`)
+console.log(`La cantidad de caracteres que tiene la cadena \n ingresada anterior mente son:  ${cadena.length}`)
+
