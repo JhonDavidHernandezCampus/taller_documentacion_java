06ea678aa4fabd10222792c395167a9844f72e87
JhonDavidHernandezCampus <yonif975@gmail.com>
2023-03-07 12:57:37 -0500

    Revert "part one"
    This reverts commit 723391cd60d426f22095ab333c5d454da30d9305.

--- documentacion.js
+++ documentacion.js
@@ -3,10 +3,10 @@ El método startsWith() indica si una cadena de texto comienza con los caractere
 una cadena de texto concreta, devolviendo true o false según corresponda.
    Ejemplo */
 
-alert("Me evalua si un caracter se encuentra en la primera posision de la cadena de texto:)")
+alert("Me evalua si un caracter se encuentra en la ultima posision de la cadena de texto:)")
 let cadena =  prompt("Ingrese una cadena de texto:")
 let letra = prompt("ingrese una letra para evaluar si se encuentra al primcipio de la cadena de texto:");
 
 
-alert(`¿la letra ' ${letra} ' se encuentra en la primera posicion de la cadena ingresada anteriormente\n  ${cadena.startsWith(letra)}`)
+alert(`¿la letra ' ${letra} ' se encuentra en la ultima posicion de la cadena ingresada anteriormente\n  ${cadena.endsWith(letra)}`)
 
