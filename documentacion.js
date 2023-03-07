c636e50d8e2deae5aea9e267e58d3c077f75c5b2
JhonDavidHernandezCampus <yonif975@gmail.com>
2023-03-07 13:10:18 -0500

    feat: {Funcion (replace)}

--- documentacion.js
+++ documentacion.js
@@ -1,12 +1,17 @@
 /* Método includes
-El método startsWith() indica si una cadena de texto comienza con los caracteres de 
-una cadena de texto concreta, devolviendo true o false según corresponda.
+El método replace() devuelve una nueva cadena con algunas o todas las coincidencias de un patrón. Como
+primer argumento, se le pasa la coincidencia que se busca y se quiere reemplazar y como segundo
+argumento, se le pasa la cadena por la cual se reemplazará la coincidencia. Si el patrón es una cadena,
+sólo la primera coincidencia será reemplazada.
    Ejemplo */
 
-alert("Me evalua si un caracter se encuentra en la ultima posision de la cadena de texto:)")
+alert("Me reemplaza una parte de la cadena indicada por otra:)")
 let cadena =  prompt("Ingrese una cadena de texto:")
-let letra = prompt("ingrese una letra para evaluar si se encuentra al primcipio de la cadena de texto:");
+let letra = prompt("ingrese la parte del texto que desea reemplazar:");
+let cambio = prompt(`ingrese el texto por el cual desea reemplazar la:${letra} `);
 
 
-alert(`¿la letra ' ${letra} ' se encuentra en la ultima posicion de la cadena ingresada anteriormente\n  ${cadena.endsWith(letra)}`)
+alert(`Me reemplaza la palabra ${letra}  por ${cambio}\n  ${cadena.replace(letra , cambio)}`)
+alert(`La cadena queda asi: \n ${cadena.replace(letra , cambio)}`)
+
 
