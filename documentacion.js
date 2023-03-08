696c8e494d9792d77c61e881e9dc5f78d931f846
JhonDavidHernandezCampus <yonif975@gmail.com>
2023-03-08 13:09:54 -0500

    feat: :sparkles: Operadores alitmeticos y ejemplos

--- documentacion.js
+++ documentacion.js
@@ -1,13 +1,14 @@
-/* Metodo toLowerCase()
+/* Operadores  
+operadores Aritmeticos
 
-Me regresa la cadena en Minuscula
+*/
 
-   Ejemplo */
+alert("Operadores De comparacion (==, !=,===,!==,<,>,>=,<=)")
 
-alert(" Me convierte toda la cadena a mainuscula:)")
-let cadena =  prompt("Ingrese una cadena de texto:")
+let num1 = Number(prompt("ingrese un numero para comparar"))
+let num2 = Number(prompt("ingrese otro numero para comparar"))
 
-
-alert(`La cadena queda de la siguiente forma : \n  ${cadena.toLocaleLowerCase()}`)
-
-
+alert(`¿los dos numeros son iguales (==)? \n\t\t ${num1 == num2} \n¿los numeros son diferentes (!=)? \n\t\t ${num1 != num2}\n
+    ¿Los numeros son iguales y son del mismo tipo de dato (===)? \n\t\t  ${num1 === num2} \n¿las variables son del mismo tipo pero no son iguales? \n\t\t ${num1 !== num2}\n
+    ¿El numero ${num1} es menor que ${num2} (<)?\n\t\t ${num1<num2}\n¿El numero ${num1} es mayor que ${num2} (>)?\n\t\t ${num1>num2}\n
+    ¿El numero ${num1} es mayor o igual que ${num2} (>=)?\n\t\t ${num1 >= num2}\n ¿El numero ${num1} es menor o igual que ${num2} (<=)?\n\t\t ${num1<=num2}`)
